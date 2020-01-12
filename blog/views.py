ecc926ec9824450656e9032d73cdfeddebec3ea2
pratik149 <pratikrane149@gmail.com>
2020-01-12 18:58:12 +0530

    Added comment feature to blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from blog.models import Post
-from blog.forms import PostForm
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
@@ -58,3 +58,29 @@ def post_remove(request, pk):
     post.delete()
     return redirect('post_list')
 
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
