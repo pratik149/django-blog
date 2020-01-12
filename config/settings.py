82cedc4eda682f44be3c3f1ebb34247d54ba66f8
pratik149 <pratikrane149@gmail.com>
2020-01-12 05:42:32 +0530

    Ran makemirgation cmd, and created superuser for admin panel

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lh#qp971)0yry5^fkb%_bxsk3znq+tae64uvl9tzmgs5n1=&h*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
